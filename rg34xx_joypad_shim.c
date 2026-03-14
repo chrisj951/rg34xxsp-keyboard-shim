329f272f96fc4c2244ab7a721b18b7bfd535f56b
chrisj951 <chrisj951@gmail.com>
2026-03-14 17:21:57 -0400

    Spoof being a 360 controller

--- rg34xx_joypad_shim.c
+++ rg34xx_joypad_shim.c
@@ -1,4 +1,5 @@
 // rg34xx_joypad_shim.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,71 +12,137 @@
 
 #define INPUT_DEV "/dev/input/event1"
 
-int setup_uinput() {
-    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-    if(fd < 0) { perror("open /dev/uinput"); return -1; }
+void emit(int fd, int type, int code, int value)
+{
+    struct input_event ev;
+    memset(&ev, 0, sizeof(ev));
 
-    // Mark device as a gamepad
+    gettimeofday(&ev.time, NULL);
+    ev.type = type;
+    ev.code = code;
+    ev.value = value;
+
+    if (write(fd, &ev, sizeof(ev)) < 0)
+        perror("write event");
+}
+
+int setup_uinput()
+{
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if (fd < 0) {
+        perror("open /dev/uinput");
+        return -1;
+    }
+
+    /* Event types */
     ioctl(fd, UI_SET_EVBIT, EV_KEY);
     ioctl(fd, UI_SET_EVBIT, EV_ABS);
-    ioctl(fd, UI_SET_PROPBIT, INPUT_PROP_DIRECT);
+    ioctl(fd, UI_SET_EVBIT, EV_SYN);
 
-    // Map all gamepad buttons RetroArch expects
-    int btns[] = {
-        BTN_SOUTH, BTN_EAST, BTN_NORTH, BTN_WEST,  // A/B/X/Y
-        BTN_TL, BTN_TR, BTN_TL2, BTN_TR2,          // L/R, L2/R2
-        BTN_START, BTN_SELECT, BTN_MODE            // Start/Select/Menu/Hotkey
+    /* Xbox 360 buttons */
+    int buttons[] = {
+        BTN_SOUTH,   // A
+        BTN_EAST,    // B
+        BTN_WEST,    // X
+        BTN_NORTH,   // Y
+        BTN_TL,      // LB
+        BTN_TR,      // RB
+        BTN_SELECT,  // Back
+        BTN_START,   // Start
+        BTN_MODE,    // Guide
+        BTN_THUMBL,  // Left stick press
+        BTN_THUMBR   // Right stick press
     };
-    for(int i = 0; i < sizeof(btns)/sizeof(btns[0]); i++)
-        ioctl(fd, UI_SET_KEYBIT, btns[i]);
 
-    // D-pad (ABS_HAT0X/Y)
-    ioctl(fd, UI_SET_ABSBIT, ABS_HAT0X);
-    ioctl(fd, UI_SET_ABSBIT, ABS_HAT0Y);
+    for (size_t i = 0; i < sizeof(buttons)/sizeof(buttons[0]); i++)
+        ioctl(fd, UI_SET_KEYBIT, buttons[i]);
+
+    /* Axes */
+    int axes[] = {
+        ABS_X, ABS_Y,
+        ABS_RX, ABS_RY,
+        ABS_Z, ABS_RZ,
+        ABS_HAT0X, ABS_HAT0Y
+    };
+
+    for (size_t i = 0; i < sizeof(axes)/sizeof(axes[0]); i++)
+        ioctl(fd, UI_SET_ABSBIT, axes[i]);
 
     struct uinput_user_dev uidev;
     memset(&uidev, 0, sizeof(uidev));
-    snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "rg34xx-shim");
-    uidev.id.bustype = BUS_USB;
-    uidev.id.vendor  = 0x1234;
-    uidev.id.product = 0x5678;
-    uidev.id.version = 1;
 
-    // D-pad range
+    snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "Microsoft X-Box 360 pad");
+
+    /* Xbox 360 USB IDs */
+    uidev.id.bustype = BUS_USB;
+    uidev.id.vendor  = 0x045e;
+    uidev.id.product = 0x028e;
+    uidev.id.version = 0x0114;
+
+    /* Left stick */
+    uidev.absmin[ABS_X] = -32768;
+    uidev.absmax[ABS_X] = 32767;
+
+    uidev.absmin[ABS_Y] = -32768;
+    uidev.absmax[ABS_Y] = 32767;
+
+    /* Right stick */
+    uidev.absmin[ABS_RX] = -32768;
+    uidev.absmax[ABS_RX] = 32767;
+
+    uidev.absmin[ABS_RY] = -32768;
+    uidev.absmax[ABS_RY] = 32767;
+
+    /* Triggers */
+    uidev.absmin[ABS_Z] = 0;
+    uidev.absmax[ABS_Z] = 255;
+
+    uidev.absmin[ABS_RZ] = 0;
+    uidev.absmax[ABS_RZ] = 255;
+
+    /* D‑pad */
     uidev.absmin[ABS_HAT0X] = -1;
     uidev.absmax[ABS_HAT0X] = 1;
+
     uidev.absmin[ABS_HAT0Y] = -1;
     uidev.absmax[ABS_HAT0Y] = 1;
 
-    if(write(fd, &uidev, sizeof(uidev)) < 0) { perror("write uinput_user_dev"); close(fd); return -1; }
-    if(ioctl(fd, UI_DEV_CREATE) < 0) { perror("UI_DEV_CREATE"); close(fd); return -1; }
+    if (write(fd, &uidev, sizeof(uidev)) < 0) {
+        perror("write uinput_user_dev");
+        close(fd);
+        return -1;
+    }
+
+    if (ioctl(fd, UI_DEV_CREATE) < 0) {
+        perror("UI_DEV_CREATE");
+        close(fd);
+        return -1;
+    }
+
+    sleep(1); // allow device node creation
 
     return fd;
 }
 
-void emit(int fd, int type, int code, int value) {
-    struct input_event ev;
-    memset(&ev, 0, sizeof(ev));
-    ev.type = type;
-    ev.code = code;
-    ev.value = value;
-    gettimeofday(&ev.time, NULL);
-    if(write(fd, &ev, sizeof(ev)) < 0) perror("write event");
-}
-
-int main() {
+int main()
+{
     int ufd = setup_uinput();
-    if(ufd < 0) return 1;
+    if (ufd < 0)
+        return 1;
 
     int ifd = open(INPUT_DEV, O_RDONLY);
-    if(ifd < 0) { perror("open input device"); return 1; }
+    if (ifd < 0) {
+        perror("open input device");
+        return 1;
+    }
 
     struct input_event ev;
-    while(1) {
+
+    while (1) {
         int rd = read(ifd, &ev, sizeof(ev));
-        if(rd == sizeof(ev)) {
-            // Forward EV_KEY and EV_ABS events
-            if(ev.type == EV_KEY || ev.type == EV_ABS) {
+
+        if (rd == sizeof(ev)) {
+            if (ev.type == EV_KEY || ev.type == EV_ABS) {
                 emit(ufd, ev.type, ev.code, ev.value);
                 emit(ufd, EV_SYN, SYN_REPORT, 0);
             }
@@ -85,5 +152,6 @@ int main() {
     ioctl(ufd, UI_DEV_DESTROY);
     close(ufd);
     close(ifd);
+
     return 0;
 }
